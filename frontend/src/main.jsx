c4b9d94f3560cd881400633960229bd040095150
Divyansh <divyanshchoudhary789@gmail.com>
2026-03-25 19:49:25 +0530

    updates

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,6 @@
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import './index.css';
+
 import { AuthProvider } from './authContext.jsx'
 import ProjectRoutes from './Routes.jsx';
 import { BrowserRouter } from 'react-router-dom';
